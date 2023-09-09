42f82c7c2737aa3e824661a5edcbfdd3f88a7297
Troy Kinsella <troy.kinsella@startmail.com>
2023-09-08 22:27:16 -0700

    n stuff

--- src/direction.rs
+++ src/direction.rs
@@ -1,7 +1,7 @@
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
-use bevy_math::{uvec2, IVec2, UVec2};
+use bevy_math::{IVec2, UVec2};
 
 pub const NORTH: IVec2 = IVec2 { x: 0, y: 1 };
 pub const NORTH_EAST: IVec2 = IVec2 { x: 1, y: 1 };
